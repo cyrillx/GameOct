1c056b0ab23f2ee94f6b548585b14eacd289a6a7
warzes <warzes@mail.ru>
2025-09-23 16:01:17 +1000

    - пересборка

---
+++ Game/Game/main.cpp
@@ -0,0 +1,21 @@
+﻿#include "stdafx.h"
+//=============================================================================
+#if defined(_MSC_VER)
+#	pragma comment( lib, "3rdparty.lib" )
+#endif
+//=============================================================================
+#if defined(_WIN32)
+extern "C"
+{
+	__declspec(dllexport) unsigned long NvOptimusEnablement = 1;
+	__declspec(dllexport) int AmdPowerXpressRequestHighPerformance = 1;
+}
+#endif
+//=============================================================================
+
+int main(
+	[[maybe_unused]] int   argc,
+	[[maybe_unused]] char* argv[])
+{
+}
+//=============================================================================

6672de34d19cac87d8a91e6aed80c2a9f25a324f
warzes <warzes@mail.ru>
2025-09-25 20:29:46 +1000

    -idea

--- Game/Game/GameApp.cpp
+++ Game/Game/GameApp.cpp
@@ -8,8 +8,9 @@
 #include "NanoOpenGL3.h"
 #include "NanoIO.h"
 // https://github.com/Keypekss/OpenGLTechDemo
-// https://github.com/vcoda/aggregated-graphics-samples
 // https://github.com/Mikepicker/opengl-sandbox
+// https://github.com/petitg1987/urchinEngine
+// https://github.com/vcoda/aggregated-graphics-samples
 // https://github.com/cforfang/opengl-shadowmapping
 // https://github.com/uobirek/opengl-3D-renderer
 // https://github.com/pulkitjuneja/GlEn
